fa00dd117aa0b23ee7674969fa859707f018fe5d
Erik Hergins <hergins1@hotmail.com>
2019-03-31 23:49:10 -0500

    final commit

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -8,7 +8,7 @@ function renderButtons() {
     for (var i = 0; i < teams.length; i++) {
 
         var newButton = $("<button>");
-        
+
         newButton.addClass("team");
         newButton.attr("data-name", teams[i]);
         newButton.text(teams[i]);
@@ -17,7 +17,7 @@ function renderButtons() {
     }
 }
 
-$("#new-team").on("click", function(event){
+$("#new-team").on("click", function (event) {
 
     event.preventDefault();
 
@@ -28,35 +28,43 @@ $("#new-team").on("click", function(event){
 
     renderButtons();
 
-$("button").on("click", function(){
-    var getGiphy = $(this).attr("data-name");
+    $("button").on("click", function () {
+        var getGiphy = $(this).attr("data-name");
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FEGmMdbgPZ6sVpLowC6U4HGt5wik5OEc&q=" + getGiphy + "&limit=10&offset=&rating=PG&lang=en";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FEGmMdbgPZ6sVpLowC6U4HGt5wik5OEc&q=" + getGiphy + "&limit=10&offset=&rating=PG&lang=en";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
-        .then(function(response){
-            console.log(response);
-            var results = response.data;
-
-            for (var i = 0; i < results.length; i++){
-
-                var teamDiv = $("<div>");
-                var rating = $("<p>").text("Rating: " + results[i].rating);
-                var teamImage = $("<img>");
-                teamImage.attr("src", results[i].images.fixed_width_still.url);
-                teamDiv.append(rating);
-                teamDiv.append(teamImage);
-
-                $("#images-here").prepend(teamDiv);
-            }
+        $.ajax({
+            url: queryURL,
+            method: "GET"
         })
 
-}
-);    
+            .then(function (response) {
+                console.log(response);
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+
+                    var teamDiv = $("<div>");
+                    var rating = $("<p>").text("Rating: " + results[i].rating);
+                    var teamImage = $("<img>");
+                    teamImage.addClass("gif");
+                    teamImage.attr("src", results[i].images.fixed_width_still.url);
+                    teamDiv.append(rating);
+                    teamDiv.append(teamImage);
+
+                    $("#images-here").prepend(teamDiv);
+                }
+                
+                // var still = results[i].images.fixed_width_still;
+                // var animated = results[i].images.fixed_height.url;
+                // $(".gif").on("click", function(){
+            })
+    }
+    );
+
 });
+
+
+})
 
 renderButtons();
